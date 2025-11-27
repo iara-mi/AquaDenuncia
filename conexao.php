1be9ff85d970cef32b0aed58e417461d61b7efb0
iara-mi <iaraduartemiguel29@gmail.com>
2025-11-27 11:57:23 -0300

    FINAL: Aplicacao do layout centralizado e correcao de warnings/estilos.

--- conexao.php
+++ conexao.php
@@ -1,16 +1,24 @@
 <?php
+/**
+ * ARQUIVO: conexao.php
+ * Define os parâmetros e estabelece a conexão com o banco de dados MySQL.
+ */
 
-$servername = "localhost";
-$username = "root"; // Deve ser root
-$password = "";     // Deve ser vazio
-$dbname = "hackathon_desperdicio"; // Nome do seu banco de dados
+// 1. Definição das variáveis de conexão (Ambiente Local - XAMPP/WAMP)
+$host = "localhost";
+$usuario = "root"; // Usuário padrão do XAMPP/WAMP
+$senha = "";       // Senha padrão do XAMPP/WAMP (deve ser vazio)
+$database = "hackathon_desperdicio"; // Nome do seu banco de dados
 
-$conn = new mysqli($host, $usuario, $senha, $dbname);
+// 2. Estabelece a conexão usando as variáveis definidas
+$conn = new mysqli($host, $usuario, $senha, $database);
 
+// 3. Verifica a conexão
 if ($conn->connect_error) {
     die("Erro na conexão com o banco de dados: " . $conn->connect_error);
 }
 
+// 4. Define o charset para evitar problemas com acentuação
 $conn->set_charset("utf8");
 
-?>
+// IMPORTANTE: Não inclua a tag de fechamento (?>) para evitar problemas de header
