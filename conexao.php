e3590f81727cc970b41be846925168fa7c7e1df7
iara-mi <iaraduartemiguel29@gmail.com>
2025-11-26 23:02:50 -0300

    Finalizacao: Atualiza credenciais de conexao para ambiente de producao (InfinityFree)

--- conexao.php
+++ conexao.php
@@ -1,14 +1,16 @@
 <?php
 
-$host = 'localhost';
-$usuario = 'root'; 
-$senha = '';
-$dbname = 'hackathon_desperdicio';
+$host = 'sql108.infinityfree.com'; 
+$usuario = 'if0_40530559';
+$senha = 'y6MFjpU2nAg';       
+$dbname = 'if0_40530559_alerta'; 
+
 
 $conn = new mysqli($host, $usuario, $senha, $dbname);
 
 if ($conn->connect_error) {
-    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+
+    die("Falha na conexão com o banco de dados. Contate o administrador.");
 }
 
 $conn->set_charset("utf8");
